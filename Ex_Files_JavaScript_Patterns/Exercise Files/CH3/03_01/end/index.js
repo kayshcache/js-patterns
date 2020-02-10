b2070bf5184bc1a97f072a72edd89950dc4b7b1a
thcl_hkr <kayshcache@github.com>
2020-02-11 09:25:46 +1100

    a small react app to look over later

--- Ex_Files_JavaScript_Patterns/Exercise Files/CH3/03_01/end/index.js
+++ Ex_Files_JavaScript_Patterns/Exercise Files/CH3/03_01/end/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import calc from './calc';
 
 const app = express();
 const PORT = 3000;
+const aNumber = calc();
 
 app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+    res.send(`Showing number ${aNumber} on port ${PORT}`)
 );
 
 app.listen(PORT, () =>
